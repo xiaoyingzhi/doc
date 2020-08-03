0dff6defac0e6f6860fc8392ac3c528b0f47691b
GitHub Action <action@github.com>
2020-08-03 10:04:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/xiaoyingzhi/doc/MyBundle.kt
+++ src/main/kotlin/com/github/xiaoyingzhi/doc/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.xiaoyingzhi.doc
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/xiaoyingzhi/doc/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xiaoyingzhi/doc/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xiaoyingzhi.doc.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xiaoyingzhi.doc.MyBundle
 
 class MyApplicationService {
 
